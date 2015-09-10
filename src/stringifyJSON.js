9f209ffe41cb97cf7582e84cf8007c19bebf7411
Kurt Weiberth <kurt.weiberth@gmail.com>
2015-09-10 14:18:43 -0700

    write stringify, made all test pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,58 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (obj === undefined || typeof obj === "function") {
+
+    return null;
+
+  } else if (obj === null) {
+
+    return "null";
+
+  } else if (Array.isArray(obj)) {
+
+    var arrayString = "[";
+
+    obj.forEach(function(item) {
+      arrayString += stringifyJSON(item) + ",";
+    });
+
+    if (obj.length) {
+      arrayString = arrayString.slice(0, -1);
+    }
+
+    return arrayString += "]";
+
+  } else if (typeof obj === "object") {
+
+    var objString = "{";
+    var validProps = 0;
+
+    for (var prop in obj) {
+      if (!(obj[prop] === undefined || typeof obj[prop] === "function")) {
+        objString += "\"" + prop + "\":" + stringifyJSON(obj[prop]) + ",";
+        validProps++;
+      }
+    }
+
+    if (validProps) {
+      objString = objString.slice(0, -1);
+    }
+
+    return objString += "}";
+
+  } else if (typeof obj === "string") {
+
+    return "\"" + obj  + "\"";
+
+  } else {
+
+    return obj.toString();
+
+  }
 };
+
+
+
+      
